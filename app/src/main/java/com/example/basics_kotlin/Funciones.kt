8145e5b57b09cf0a4b1c5ff0f1efa07407ba06ac
Mike <miguel.vergara@nanerobotics.com>
2022-05-13 16:36:52 -0500

    Infix y sobre carga

--- app/src/main/java/com/example/basics_kotlin/Funciones.kt
+++ app/src/main/java/com/example/basics_kotlin/Funciones.kt
@@ -1,5 +1,7 @@
 package com.example.basics_kotlin
 
+import java.lang.Math.abs
+
 fun main(){
     sayHello()
     newTopic("Argumentos")
@@ -7,6 +9,19 @@ fun main(){
     val b = 3
     println("$a + $b = ${sum(a,b)}")
     println("$a - $b = ${sub(a,b)}")
+
+    newTopic("Infix")
+    val c = -3
+    println(c.enableAbs(true))
+    println("$a + $c = ${sum(a,c.enableAbs(false))}")
+    println("$a + $c = ${sum(a,c.enableAbs(true))}")
+
+    newTopic("Sobrecarga")
+    showProduct("Soda","10%")
+    showProduct("Icecream","15%","5 de Julio")
+    showProduct("Jelly Beans")
+    showProduct(name = "Nerds", promo = "20%", validity = "30 de mayo")
+
 }
 
 private fun sayHello(): Unit {  //private means qeu solo se puede usar en este archivo el metodo y unit es equivalente a void
@@ -18,3 +33,9 @@ fun sum(a:Int, b:Int): Int{
 }
 
 fun sub(a: Int,b: Int) = a -b //esta opcion tiene el return implicito sin embargo solo es conveniente para fun muy simples
+
+infix fun Int.enableAbs(enable: Boolean) = if (enable) abs(this) else this
+
+fun showProduct(name: String, promo: String = "Sin promo", validity:String= "agotar existencias"){
+    println("$name = $promo ;Hasta $validity")
+}
